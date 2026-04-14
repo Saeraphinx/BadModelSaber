bbfc801b5e47af8c2dcc6392b71273521e07d64c
Saera <saeraphinx@gmail.com>
2026-04-14 09:31:55 -0500

    add fetch logging

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,15 +1,28 @@
-import type { Handle } from '@sveltejs/kit';
+import type { Handle, HandleFetch } from '@sveltejs/kit';
 import { paraglideMiddleware } from '$lib/paraglide/server';
+import { env } from '$env/dynamic/public';
+import { browser } from '$app/environment';
 
 // creating a handle to use the paraglide middleware
 const paraglideHandle: Handle = ({ event, resolve }) =>
-	paraglideMiddleware(event.request, ({ request: localizedRequest, locale }) => {
-		event.request = localizedRequest;
-		return resolve(event, {
-			transformPageChunk: ({ html }) => {
-				return html.replace('%lang%', locale);
-			}
-		});
-	});
+  paraglideMiddleware(event.request, ({ request: localizedRequest, locale }) => {
+    event.request = localizedRequest;
+    return resolve(event, {
+      transformPageChunk: ({ html }) => {
+        return html.replace('%lang%', locale);
+      }
+    });
+  });
 
 export const handle: Handle = paraglideHandle;
+
+export const handleFetch: HandleFetch = async ({ request, fetch, event }) => {
+	return fetch(request).then((response) => {
+    console.log(`Fetch request to ${request.url} returned with status ${response.status}`);
+    console.log(response);
+    return response;
+  }).catch((err) => {
+    console.error(`Fetch request to ${request.url} failed with error:`, err);
+    throw err;
+  });
+};
