eb4641cd65b5491ec68ec05bbccac13ff6db4ca7
Saera <saeraphinx@gmail.com>
2026-04-14 16:09:01 -0500

    remove logging

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -2,7 +2,7 @@ import type { Handle, HandleFetch } from '@sveltejs/kit';
 import { paraglideMiddleware } from '$lib/paraglide/server';
 import { env } from '$env/dynamic/public';
 import { env as penv } from '$env/dynamic/private';
-import { browser } from '$app/environment';
+import { browser, dev } from '$app/environment';
 
 // creating a handle to use the paraglide middleware
 const paraglideHandle: Handle = ({ event, resolve }) =>
@@ -21,11 +21,10 @@ export const handleFetch: HandleFetch = async ({ request, fetch: svelteFetch, ev
   let modifiedRequest = request;
   let fetchToUse = svelteFetch;
   if (!browser && request.url.startsWith(env.PUBLIC_API_URL)) {
-    // https://github.com/sveltejs/kit/issues/8314
-    console.log(`Modifying request to ${request.url}`);
-    
+      
     // replace url with local version if the request is going to the API, no need to hit the proxy
-    let url = penv.LOCAL_API_URL ? request.url.replace(env.PUBLIC_API_URL, penv.LOCAL_API_URL || env.PUBLIC_API_URL) : request.url;
+    let url = new URL(penv.LOCAL_API_URL ? request.url.replace(env.PUBLIC_API_URL, penv.LOCAL_API_URL || env.PUBLIC_API_URL) : request.url);
+    if (event.url.origin !== url.origin && dev) console.warn(`Request to ${url.origin} is going to a different origin than the server (${event.url.origin} vs ${url.origin}). This request will require CORS.`);  
     modifiedRequest = new Request(url, {
       headers: {
         ...request.headers,
@@ -36,10 +35,10 @@ export const handleFetch: HandleFetch = async ({ request, fetch: svelteFetch, ev
     //fetchToUse = fetch;
   }
 	return fetchToUse(modifiedRequest).then((response) => {
-    console.log(`Fetch request to ${request.url} returned with status ${response.status}`);
-    console.log(event);
-    console.log(modifiedRequest);
-    console.log(response);
+    //console.log(`Fetch request to ${request.url} returned with status ${response.status}`);
+    //console.log(event);
+    //console.log(modifiedRequest);
+    //console.log(response);
     return response;
   }).catch((err) => {
     console.error(`Fetch request to ${request.url} failed with error:`, err);
